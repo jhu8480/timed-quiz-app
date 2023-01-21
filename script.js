9cd1aadd9e695dc12480b5e3d896be456e8b7525
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-01-20 21:33:21 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//variables
+const startButton = document.getElementById('start');
+const viewScoreBtn = document.querySelector('#view-highscore');
+const timeDisplay = document.querySelector('#time-display');
+
+startButton.addEventListener('click', startTheQuiz);
+
+
+//execution
+timeDisplay.setAttribute('style', 'opacity: 0');
+
+//functions
+
+function startTheQuiz() {
+  
+}
