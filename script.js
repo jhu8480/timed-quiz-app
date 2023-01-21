48e4d1276028ef0862fb2101d198ecb56d0d175d
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-01-20 22:41:30 -0500

    added quiz questions and answers list inner html to container

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 //variables
-import questions from "./data";
+import questions from "./data.js";
+const container = document.getElementById('container');
 const startButton = document.getElementById('start');
 const viewScoreBtn = document.querySelector('#view-highscore');
 
-
 const timeDisplay = document.querySelector('#time-display');
 startButton.addEventListener('click', startTheQuiz);
+
+let timeLeft = 60;
+let currentQuestion = 0;
 
 
 
@@ -16,5 +19,50 @@ timeDisplay.setAttribute('style', 'opacity: 0');
 //functions
 
 function startTheQuiz() {
+  timeDisplay.setAttribute('style', 'display: default');
+  timeDisplay.innerText = `Time left: 60`;
+  let timeInterval = setInterval(() => {
+    if (timeLeft > -1) {
+      timeLeft--;
+      timeDisplay.innerText = `Time left: ${timeLeft}`;
+    } else {
+      clearInterval(timeInterval);
+    }
+  }, 1000);
 
+  renderQuestion();
+}
+
+function renderQuestion() {
+  container.innerHTML = 
+    `<h2>Question ${currentQuestion + 1}</h2>
+    <p>${questions[currentQuestion].question}</p>
+    <ul>
+      <li>
+      <input type="radio" id="a" name="choice">
+      <label for="a">${questions[currentQuestion].a}<label>
+      </li>
+      <li>
+      <input type="radio" id="b" name="choice">
+      <label for="a">${questions[currentQuestion].b}<label>
+      </li>
+      <li>
+      <input type="radio" id="c" name="choice">
+      <label for="a">${questions[currentQuestion].c}<label>
+      </li>
+      <li>
+      <input type="radio" id="d" name="choice">
+      <label for="a">${questions[currentQuestion].d}<label>
+      </li>
+    </ul>
+    <button id="submit-question">Submit</button>`;
+    const submitButton = document.querySelector('#submit-question');
+    submitButton.addEventListener('click', nextQuestion);
+}
+
+function nextQuestion() {
+  if (currentQuestion == questions.length - 1) return;
+
+  currentQuestion++;
+  renderQuestion();
 }
